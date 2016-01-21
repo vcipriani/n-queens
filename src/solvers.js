db1940f71d8c2f48e410415b1772b22d644674d6
vcipriani <vince.cipriani@gmail.com>
2016-01-21 12:31:48 -0800

    Refactored subroutine in findFirstQueen/Rook solution to separate function

--- src/solvers.js
+++ src/solvers.js
@@ -16,92 +16,28 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solutionBoard;
-
-  var placePieces = function(board, row) {
-    if(row === n - 1) {
-      for(var col = 0; col < n; col++) {
-        board.togglePiece(row, col);
-        if(!board.hasAnyRooksConflicts()) {
-          solutionBoard = board;
-        } else {
-          board.togglePiece(row, col);
-        }
-      }
-    } else {
-      for(var col = 0; col < n; col++) {
-        board.togglePiece(row,col);
-        if(!board.hasAnyRooksConflicts()) {
-          placePieces(board, row + 1);
-          if(solutionBoard) {
-            break;
-          }
-        } else {
-          board.togglePiece(row,col);
-        }
-      }
-    }
-  };
-
   var newBoard = new Board({n: n});
-  placePieces(newBoard, 0);
-
-
-
-
+  var solutionBoard = findFirstSolution(newBoard, 0, {}, newBoard.hasAnyRooksConflicts);
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solutionBoard.rows()));
   return solutionBoard.rows();
 };
 
+
+
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
-  
+window.countNRooksSolutions = function(n) {  
   var newBoard = new Board({n: n});
- 
   var solutionCount = placePieces(newBoard, 0, {}, newBoard.hasAnyRooksConflicts);
-
-
-
-
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
+
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-
-
-  var solutionBoard;
-  var solution;
-  var placePieces = function(board, row) {
-    if(row === n - 1) {
-      for(var col = 0; col < n; col++) {
-        board.togglePiece(row, col);
-        if(!board.hasAnyQueensConflicts()) {
-          solutionBoard = board;
-        } else {
-          board.togglePiece(row, col);
-        }
-      }
-    } else {
-      for(var col = 0; col < n; col++) {
-        board.togglePiece(row,col);
-        if(!board.hasAnyQueensConflicts()) {
-          placePieces(board, row + 1);
-          if(solutionBoard) {
-            break;
-          }
-        } 
-        board.togglePiece(row,col);
-      }
-    }
-  };
-
   var newBoard = new Board({n: n});
-  placePieces(newBoard, 0);
+  var solutionBoard = findFirstSolution(newBoard, 0, {}, newBoard.hasAnyQueensConflicts);
   solution = solutionBoard ? solutionBoard.rows() : (new Board({n: n})).rows();
-
-
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
@@ -111,14 +47,43 @@ window.countNQueensSolutions = function(n) {
   if( n === 0 ) {
     return 1;
   }
-  
   var newBoard = new Board({n: n});
-  
   var solutionCount = placePieces(newBoard, 0, {}, newBoard.hasAnyQueensConflicts);
-
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
+
+var findFirstSolution = function(board, row, colsUsed, conflictFn) {
+  if(row === board.rows().length - 1) {
+    for(var col = 0; col < board.rows().length; col++) {
+      if(!(col in colsUsed)) {
+        board.togglePiece(row, col);
+        if(!conflictFn.call(board)) {
+          return board;
+        } else {
+          board.togglePiece(row, col);
+          return null;
+        }
+      }
+    }
+  } else {
+    for(var col = 0; col < board.rows().length; col++) {
+      if(!(col in colsUsed)) {
+        board.togglePiece(row,col);
+        colsUsed[col] = col;
+        if(!conflictFn.call(board)) {
+          var solutionBoard = findFirstSolution(board, row + 1, colsUsed, conflictFn);
+          if(solutionBoard) {
+            return solutionBoard;
+          }
+        } 
+        board.togglePiece(row,col);
+        delete colsUsed[col];
+      }
+    }
+  }
+};
+
 
 var placePieces = function(board, row, colsUsed, conflictFn) {
   if(row === board.rows().length - 1) {
@@ -152,6 +117,7 @@ var placePieces = function(board, row, colsUsed, conflictFn) {
     return sum;
   }
 };
+
 
 window.countNQueensBitwise = function(n) {
   var ones = (1 << n) - 1;
